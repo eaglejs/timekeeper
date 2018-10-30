5ccc01258da3e9cb2dc21e64da1960bd68b61828
eaglejs <joshuaseagle@gmail.com>
2018-10-30 15:42:29 -0400

    formatting code.

--- deltek-timesheet.rb
+++ deltek-timesheet.rb
@@ -1,47 +1,47 @@
-require 'json'
-require_relative 'site-element.rb'
+require "json"
+require_relative "site-element.rb"
 
 file = File.read "./config.json"
 data = JSON.parse(file)
 
 ARGV.each do |argument|
-	if /time/.match(argument.downcase)[0] == "time"
-		TIMEINPUT = /\d\.\d|\d/.match(argument)
-	end
+  if /time/.match(argument.downcase)[0] == "time"
+    TIMEINPUT = /\d\.\d|\d/.match(argument)
+  end
 end
 
 # define new browser
-browser = SiteElement.new(data['url'])
+browser = SiteElement.new(data["url"])
 
 wait = Selenium::WebDriver::Wait.new(timeout: 60)
 
 # click on submit button
-browser.msUserName.send_keys(data['username'])
+browser.msUserName.send_keys(data["username"])
 browser.msSubmit.click
 
-wait.until {browser.msUserPass}
+wait.until { browser.msUserPass }
 
 # Login
-browser.msUserPass.send_keys(data['password'])
+browser.msUserPass.send_keys(data["password"])
 browser.msSubmitLogin.click
 
 # Verify two-step login
-wait.until {browser.verificationLink}
+wait.until { browser.verificationLink }
 
 browser.verificationLink.click
 
-wait.until {browser.msRememberMeYes}
+wait.until { browser.msRememberMeYes }
 browser.msRememberMeYes.click
 
 # Wait for costPoint login to show up
-wait.until {browser.costPointSystemInput}
+wait.until { browser.costPointSystemInput }
 
 # Fill out Creds for Cost Point
-browser.costPointSystemInput.send_keys('UNISYS')
+browser.costPointSystemInput.send_keys("UNISYS")
 browser.costPointLoginBtn.click
 
 # Wait until you are inside of Deltek
-wait.until {browser.costPointNavTC }
+wait.until { browser.costPointNavTC }
 
 # Navigate to the timesheet
 browser.costPointNavTC.click
@@ -51,40 +51,40 @@ browser.costPointManageTimesheets.click
 
 # If it is Monday, put in timecode
 if DAY == 3
-	sleep 5
-	browser.costPointNewBtn.click
-	wait.until {browser.costPointNewTimeCodeSlot}
-	browser.costPointNewTimeCodeSlot.click
-	sleep 3
-	browser.costPointNewTimeCodeSlot.clear()
-	browser.costPointNewTimeCodeSlot.send_keys(data['timecode'])
-	sleep 3
-	browser.costPointNewPayType.send_keys(data['payType'])
-	sleep 3
-	browser.costPointNewTimeSlot.click
-	sleep 5
-	browser.costPointNewTimeSlot.clear()
-	browser.costPointNewTimeSlot.send_keys(TIMEINPUT)	
+  sleep 5
+  browser.costPointNewBtn.click
+  wait.until { browser.costPointNewTimeCodeSlot }
+  browser.costPointNewTimeCodeSlot.click
+  sleep 3
+  browser.costPointNewTimeCodeSlot.clear()
+  browser.costPointNewTimeCodeSlot.send_keys(data["timecode"])
+  sleep 3
+  browser.costPointNewPayType.send_keys(data["payType"])
+  sleep 3
+  browser.costPointNewTimeSlot.click
+  sleep 5
+  browser.costPointNewTimeSlot.clear()
+  browser.costPointNewTimeSlot.send_keys(TIMEINPUT)
 else
-	# Wait until timeslot is visible
-	wait.until {browser.costPointTimeSlot}
-	# set focus to time slot based on date
-	browser.costPointTimeSlot.click
-	sleep 3
-	# Input time, then save
-	browser.costPointTimeSlot.clear()
-	browser.costPointTimeSlot.send_keys(TIMEINPUT)
+  # Wait until timeslot is visible
+  wait.until { browser.costPointTimeSlot }
+  # set focus to time slot based on date
+  browser.costPointTimeSlot.click
+  sleep 3
+  # Input time, then save
+  browser.costPointTimeSlot.clear()
+  browser.costPointTimeSlot.send_keys(TIMEINPUT)
 end
-	
+
 sleep 2
 
 browser.costPointSave.click
 
 if DAY == 7
-	sleep 10
-	browser.costPointSign.click
-	sleep 5
-	browser.costPointConfirmSign
+  sleep 10
+  browser.costPointSign.click
+  sleep 5
+  browser.costPointConfirmSign
 end
 
 sleep 10
--- site-element.rb
+++ site-element.rb
@@ -1,96 +1,96 @@
-require 'selenium-webdriver'
-require 'io/console'
+require "selenium-webdriver"
+require "io/console"
 
-TODAY = /([A-z])\w+/.match(Time.now.strftime('%a, %m'))[0]
+TODAY = /([A-z])\w+/.match(Time.now.strftime("%a, %m"))[0]
 
 DAY = case TODAY
-    when 'Sat' then 1
-    when 'Sun' then 2
-    when 'Mon' then 3
-    when 'Tue' then 4
-    when 'Wed' then 5
-    when 'Thu' then 6
-    when 'Fri' then 7
-end
+      when "Sat" then 1
+      when "Sun" then 2
+      when "Mon" then 3
+      when "Tue" then 4
+      when "Wed" then 5
+      when "Thu" then 6
+      when "Fri" then 7
+      end
 
 class SiteElement
   def initialize(url)
-	current_window = ''
-	options = Selenium::WebDriver::Chrome::Options.new
-	@driver = Selenium::WebDriver.for :chrome, options: options
-	#@driver = Selenium::WebDriver.for :chrome
+    current_window = ""
+    options = Selenium::WebDriver::Chrome::Options.new
+    @driver = Selenium::WebDriver.for :chrome, options: options
+    #@driver = Selenium::WebDriver.for :chrome
     # @driver.manage.window.maximize
     target_position = Selenium::WebDriver::Point.new(0, 0)
     #target_size = Selenium::WebDriver::Dimension.new(840, 1050)
     @driver.manage.window.position = target_position
-	current_window = @driver.window_handle()
-	@driver.switch_to.window(current_window)
-	@driver.manage.window.maximize()
-	@driver.navigate.to url
+    current_window = @driver.window_handle()
+    @driver.switch_to.window(current_window)
+    # @driver.manage.window.maximize()
+    @driver.navigate.to url
   end
 
   def msUserName
-    @driver.find_element(:id, 'i0116')
+    @driver.find_element(:id, "i0116")
   end
 
   def msSubmit
-    @driver.find_element(:id, 'idSIButton9')
+    @driver.find_element(:id, "idSIButton9")
   end
 
   def msUserPass
-    @driver.find_element(:id, 'passwordInput')
+    @driver.find_element(:id, "passwordInput")
   end
 
   def msSubmitLogin
-    @driver.find_element(:id, 'submitButton')
-  end 
-  
+    @driver.find_element(:id, "submitButton")
+  end
+
   def msRememberMeYes
-	@driver.find_element(:id, 'idSIButton9')
+    @driver.find_element(:id, "idSIButton9")
   end
 
   def verificationLink
-    @driver.find_element(:id, 'verificationOption3')
+    @driver.find_element(:id, "verificationOption3")
   end
 
   def costPointSystemInput
-    @driver.find_element(:id, 'DATABASE')
+    @driver.find_element(:id, "DATABASE")
   end
 
   def costPointLoginBtn
-    @driver.find_element(:id, 'loginBtn')
+    @driver.find_element(:id, "loginBtn")
   end
 
   def costPointNavTC
-    @driver.find_element(:id, 'navTC')
+    @driver.find_element(:id, "navTC")
   end
 
   def costPointTimeBtn
-    @driver.find_element(:id, 'dpt__TM')
+    @driver.find_element(:id, "dpt__TM")
   end
 
   def costPointTimeSheetsBtn
-    @driver.find_element(:id, 'wrk__Timesheets')
+    @driver.find_element(:id, "wrk__Timesheets")
   end
 
   def costPointManageTimesheets
-    @driver.find_element(:id, 'actvty__TMMTIMESHEET')
+    @driver.find_element(:id, "actvty__TMMTIMESHEET")
   end
 
   def costPointTimeSlot
-    @driver.find_element(:id, format('DAY%s_HRS-_0_E', DAY))
+    @driver.find_element(:id, format("DAY%s_HRS-_0_E", DAY))
   end
 
   def costPointNewTimeSlot
-    @driver.find_element(:id, format('DAY%s_HRS-_0_N', DAY))
+    @driver.find_element(:id, format("DAY%s_HRS-_0_N", DAY))
   end
 
   def costPointSave
-    @driver.find_element(:id, 'svBttn')
+    @driver.find_element(:id, "svBttn")
   end
 
   def costPointNewBtn
-    @driver.find_elements(:class, 'rsltst')[1].find_element(:id, 'newBttn')
+    @driver.find_elements(:class, "rsltst")[1].find_element(:id, "newBttn")
   end
 
   def costPointNewLine
@@ -98,23 +98,23 @@ class SiteElement
   end
 
   def costPointTimeCodeSlot
-    @driver.find_element(:id, 'UDT02_ID-_0_E')
+    @driver.find_element(:id, "UDT02_ID-_0_E")
   end
 
   def costPointNewTimeCodeSlot
-    @driver.find_element(:id, 'UDT02_ID-_0_N')
+    @driver.find_element(:id, "UDT02_ID-_0_N")
   end
 
   def costPointPayType
-    @driver.find_element(:id, 'UDT10_ID-_0_E')
+    @driver.find_element(:id, "UDT10_ID-_0_E")
   end
 
   def costPointNewPayType
-    @driver.find_element(:id, 'UDT10_ID-_0_N')
+    @driver.find_element(:id, "UDT10_ID-_0_N")
   end
 
   def costPointSign
-    @driver.find_element(:id, 'SIGN_BUT')
+    @driver.find_element(:id, "SIGN_BUT")
   end
 
   def costPointConfirmSign
