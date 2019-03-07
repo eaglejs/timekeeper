52c136add243374007c2d884486ffefc6d407073
eaglejs <joshuaseagle@gmail.com>
2019-03-07 17:20:56 -0500

    Fix till I can figure out a better way 4 cookies.

---
+++ get-cookies.js
@@ -0,0 +1,55 @@
+class Cookies {
+  constructor() {
+    this.COOKIES = [];
+    this.initializeCookies();
+  }
+
+  initializeCookies() {
+    let cookiesEl = document
+      .querySelector('settings-ui')
+      .shadowRoot.querySelector('#main')
+      .shadowRoot.querySelector('.showing-subpage')
+      .shadowRoot.querySelector('settings-privacy-page')
+      .shadowRoot.querySelector('site-data-details-subpage')
+      .shadowRoot.querySelectorAll('iron-collapse');
+
+    this.COOKIES = [];
+
+    cookiesEl.forEach(cookieEl => {
+      let cookieElText = cookieEl.innerText.split('\n');
+      let cookieResult = {};
+
+      for (let i = 0; i < cookieElText.length - 1; i += 2) {
+        cookieElText.forEach((item, index) => {
+          if (index % 2 === 0) {
+            cookieResult[item] = cookieElText[index + 1];
+          }
+        });
+      }
+
+      this.COOKIES.push(cookieResult);
+    });
+  }
+
+  getCookies() {
+    const NEW_COOKIES = [];
+    this.COOKIES.forEach(cookie => {
+      let NEW_COOKIE = {};
+      if (cookie.Name) {
+        NEW_COOKIE.name = cookie.Name || '';
+        NEW_COOKIE.value = cookie.Content || '';
+        NEW_COOKIE.domain = cookie.Domain || '';
+        NEW_COOKIE.expirationDate = cookie.Expires || '';
+        NEW_COOKIE.path = cookie.Path || '';
+        NEW_COOKIE.HTTP = true;
+        NEW_COOKIE.secure =
+          cookie['Send for'] === 'Secure connections only'
+            ? true
+            : false;
+
+        NEW_COOKIES.push(NEW_COOKIE);
+      }
+    });
+    return NEW_COOKIES;
+  }
+}
