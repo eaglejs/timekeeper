7cb1e77628a09ab2c5255e856ce2e71f2a93cc93
Joshua S. Eagle <josh.eagle@piscesworldwide.com>
2022-04-05 10:34:27 -0400

    Fixed issue where 09 is not treated as base 10

--- do-timesheet.sh
+++ do-timesheet.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 source $HOME/.profile
 
-H=$(date +%H)
+H=$(date +%-H)
 
 if (( $H < 15 )); then
     echo "It's too early in the day to be running the script... Exiting..."
     exit 0;
 fi
 
-
-cd $HOME/repos/timekeeper/ && /usr/bin/ruby deltek-timesheet.rb time=8
+#cd $HOME/repos/timekeeper/ && /usr/bin/ruby deltek-timesheet.rb time=8
