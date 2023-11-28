7a1fc2afb116d4e8a8eb45d6430f4569ce720807
Joshua S. Eagle <joshuaseagle@gmail.com>
2023-11-28 10:06:34 -0500

    added back commented code

--- do-timesheet.sh
+++ do-timesheet.sh
@@ -4,9 +4,9 @@ source $HOME/.profile
 H=$(date +%-H)
 DO_NOT_RUN_TODAY=false
 
-#if [[ $DO_NOT_RUN_TODAY == true || $H < 15 ]]; then
-#    echo "It's too early in the day to be running the script... Exiting..."
-#    exit 0;
-#fi
+if [[ $DO_NOT_RUN_TODAY == true || $H < 15 ]]; then
+    echo "It's too early in the day to be running the script... Exiting..."
+    exit 0;
+fi
 
 cd $HOME/repos/timekeeper/ && /usr/local/bin/python3 deltek_timesheet.py
